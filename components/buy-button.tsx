1b505563d7d5311879bd2c593527ba13cba78546
yomjsty <oweychanges@gmail.com>
2025-05-18 22:52:17 +0700

    fixing webhooks

--- components/buy-button.tsx
+++ components/buy-button.tsx
@@ -12,10 +12,6 @@ export default function BuyButton() {
         });
     }
 
-    const handleState = async () => {
-        await authClient.customer.state()
-    }
-
     return (
         <div className="flex items-center justify-center gap-2 flex-wrap">
             <Button onClick={() => handleBuyCoins("d2f14d56-eb91-4466-ab2e-501a9755ae7f", "10-coins")}>
@@ -27,10 +23,6 @@ export default function BuyButton() {
                 Buy 30 Coins
             </Button>
             <Button onClick={() => handleBuyCoins("05f8b341-2e93-44c6-9aad-b7b4c879ea13", "70-coins")}>
-                <Coins className="w-4 h-4 mr-2" />
-                Buy 70 Coins
-            </Button>
-            <Button onClick={() => handleState()}>
                 <Coins className="w-4 h-4 mr-2" />
                 Buy 70 Coins
             </Button>
