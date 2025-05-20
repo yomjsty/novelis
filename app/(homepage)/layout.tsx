7d85fcceae2fadf33571a9d146e17e57478d4025
yomjsty <oweychanges@gmail.com>
2025-05-20 22:59:46 +0700

    fetching issues v9

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -1,15 +1,11 @@
-import { getCurrentUser } from "@/lib/get-current-user";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
-import { notFound } from "next/navigation";
 
 export default async function PublicLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const user = await getCurrentUser();
-    if (!user) return notFound();
 
     return (
         <div className="flex flex-col mx-auto">
--- app/(homepage)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export default function Loading() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <Loader2 className="w-14 h-14 animate-spin" />
-        </div>
-    )
-}
